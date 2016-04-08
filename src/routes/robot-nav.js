e64cc1b3559d4d7b25b3053d48e2362a49e9d9e2
Laszlo Vadasz <laszlo.vadasz@adgistics.com>
2016-04-08 11:21:42 +0100

    add API documentation

--- src/routes/robot-nav.js
+++ src/routes/robot-nav.js
@@ -8,18 +8,21 @@ module.exports = (prefix) => ({
 	method: 'POST',
 	path: `${prefix}/robot`,
 	config: {
+		description:
+			'Calculate zero or more robots location in a rectangle area ' +
+			'based on the area size and the robots position, heading and move actions.',
 		validate: {
 			payload: Joi.object().keys({
-				width: Joi.number().integer().min(1),
-				height: Joi.number().integer().min(1),
+				width: Joi.number().integer().min(1).description('Width of the fight area'),
+				height: Joi.number().integer().min(1).description('Height of the fight area'),
 				robots: Joi.array().max(1024).items(
 					Joi.object().keys({
-						x: Joi.number().integer().min(0),
-						y: Joi.number().integer().min(0),
-						heading: Joi.string().regex(/N|E|S|W/),
-						actions: Joi.string().max(1024)
+						x: Joi.number().integer().min(0).description('Robot start position x coordinate'),
+						y: Joi.number().integer().min(0).description('Robot start position y coordinate'),
+						heading: Joi.string().regex(/^(N|E|S|W)$/).description('Robot start heading (North, East, South or West)'),
+						actions: Joi.string().regex(/^(M|R|L)*$/).max(1024).description('Robot actions (Move, Left or Right)')
 					})
-				)
+				).description('List of robots')
 			})
 		},
 		handler: (req, reply) => {
